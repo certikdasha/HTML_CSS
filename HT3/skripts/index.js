05192be2663ae38bb4b309aa9cd7ccd4ab3fca25
Daria <chertik.daha@gmail.com>
2021-01-18 17:20:24 +0200

    Ht-2 vs Ht-3

---
+++ HT3/skripts/index.js
@@ -0,0 +1,28 @@
+//1
+let x = -5;
+
+if (x > 0){
+    console.log('Число положительное')
+}
+else{
+    console.log('Число отрицательное')
+}
+//2
+let summa = 0;
+let num = 0;
+
+for (let i = 0; i < 10; i++){
+    num += 5;
+    summa += num;
+}
+
+// console.log(summa);
+//3
+var numbers = [ 254, 115, 78, 25, 91, 45, 37 ]
+for (i = 0; i < numbers.length; i++){
+    if (numbers[i]>50){
+        console.log(numbers[i])
+    }
+}
+
+
